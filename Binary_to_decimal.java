0e0628adcf4ca121cf8ac2b3f336aa9d2146d27c
Adiitya786 <adityaprajapatii786@gmail.com>
2025-09-14 11:29:44 +0530

    Program that convert binary to decimal and decimal to binary using recursion.

--- Binary_to_decimal.java
+++ Binary_to_decimal.java
@@ -6,9 +6,19 @@ public class Binary_to_decimal {
         }
          return binDec(n/10, ans+(n%10*(int)Math.pow(2,p)), p+1);
     }
+
+    public static void decBin(int n,int ans,int p){
+          if(n ==0){
+            System.out.print(ans);
+            return;
+          }
+          decBin(n/2,ans+ (n%2)*(int)Math.pow(10,p),p+1);
+         
+    }
     public static void main(String[] args) {
         int  n =10001;
         int ans =0, power =0;
         System.out.println(binDec(n,ans,power));
+        decBin(17,0,0);
 }
 }
